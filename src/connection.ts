5218a12b5db5a5cb254f6a16d79bcb2ec72b2143
Stanley Masinde <stanmasinde@gmail.com>
2020-12-09 19:22:54 +0300

    Added the testing option in config

--- src/connection.ts
+++ src/connection.ts
@@ -5,5 +5,5 @@ import knex from 'knex'
 console.log(process.cwd());
 
 //@ts-ignore
-const connection = knex(process.env.NODE_ENV === 'development' ? config.development : config.production)
+const connection = knex(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'testing' ? config.development : config.production)
 export default connection
